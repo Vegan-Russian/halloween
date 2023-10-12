d2ebf8ec1da355f6f0aea028612cfde6567ef954
seal <osipov.pv1000@gmail.com>
2023-10-12 04:06:55 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = function (_, argv) {
           template: "src/pug/" + filename + ".pug",
           filename: filename + ".html",
           templateParameters: {
-              baseURL  // передача baseURL в Pug-шаблоны
+              baseURL  // передача в Pug-шаблоны
           }
         })
       );
